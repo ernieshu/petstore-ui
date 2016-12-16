7665bf2e88d62d0c05f2c45ec430b4851a28845d
Ernie Shu <ernie.shu@gmail.com>
2016-12-16 18:53:45 -0500

    ES - cleanup of delete view.

--- app/scripts/controllers/delete.js
+++ app/scripts/controllers/delete.js
@@ -2,17 +2,18 @@
 angular.module('petstoreUiApp')
   .controller('DeleteCtrl', function ($scope, $http) {
     this.deleteAPet = function(data) {
+
       $http.delete('pet/' + this.deletePetId)
-        .success( function(data) {
-          $scope.feedbackMessage = 'Successfully deleted pet';
-        })
-        .error(function(data, status, headers, config){
-          if (status===404) {
-            $scope.feedbackMessage = 'No pet found';
-          }
-          else {
-            $scope.feedbackMessage = 'System error';
-          }
+        .then( function(data) {
+                  $scope.feedbackMessage = 'Successfully deleted pet';
+               },
+               function(data) {
+                  if (data.status===404) {
+                    $scope.feedbackMessage = 'No pet found';
+                  }
+                  else {
+                    $scope.feedbackMessage = 'System error';
+                  }
         });
     };
   });
