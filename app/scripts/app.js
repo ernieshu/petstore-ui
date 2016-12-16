be1483432d74079d6a06e09b567e1957cf244da5
Ernie Shu <ernie.shu@gmail.com>
2016-12-16 17:58:46 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name petstoreUiApp
+ * @description
+ * # petstoreUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('petstoreUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'checklist-model',
+    'ngTagsInput',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/create.html',
+        controller: 'CreateCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/delete', {
+        templateUrl: 'views/delete.html',
+        controller: 'DeleteCtrl'
+      })
+      .when('/view', {
+        templateUrl: 'views/view.html',
+        controller: 'ViewCtrl'
+      })
+      .when('/create', {
+        templateUrl: 'views/create.html',
+        controller: 'CreateCtrl'
+      })
+      .otherwise({
+        redirectTo: '/about'
+      });
+  });
---
+++ app/scripts/controllers/create.js
@@ -0,0 +1,62 @@
+'use strict';
+angular.module('petstoreUiApp')
+  .controller('CreateCtrl', function ($scope, $http) {
+
+    console.log('I am here')
+
+    this.categories = [
+      { id: 0, name: 'Category 0'},
+      { id: 1, name: 'Category 1'},
+      { id: 2, name: 'Category 2'},
+      { id: 3, name: 'Category 3'}
+    ];
+
+    this.tags = [
+      { id: 0, name: 'Tag 0'},
+      { id: 1, name: 'Tag 1'},
+      { id: 2, name: 'Tag 2'},
+      { id: 3, name: 'Tag 3'}
+    ];
+
+    this.pet = {
+    };
+
+    this.feedbackMessage;
+
+    this.addAPet = function() {
+
+      // pre-process the photoUrls, so that only strings are passed, rather than full objects
+      var photoUrlStrings = [];
+      if (typeof this.pet.photoUrls!= 'undefined') {
+        photoUrlStrings = this.pet.photoUrls.map(function(photoUrl) { return photoUrl.text; });
+      };
+
+      var petToBeAdded = {
+        name : this.pet.name
+      };
+
+      // only populate certain items if they've been entered
+      if (this.pet.status!==null) {
+        petToBeAdded['status'] = this.pet.status.toUpperCase();
+      };
+      if (this.pet.categoryId!=null) {
+        petToBeAdded['category'] = { id: this.pet.categoryId };
+      };
+      if (typeof this.pet.tags!= 'undefined') {
+        petToBeAdded['tags'] = this.pet.tags;
+      };
+      if (photoUrlStrings.length > 0) { 
+        petToBeAdded['photoUrls'] = photoUrlStrings;
+      };
+
+      $http.post('pet', petToBeAdded)
+        .then(function(data, status, headers, config) {
+                $scope.feedbackMessage = 'Entity created with id:' + data.id;
+              },
+              function(data, status, headers, config){
+                if (status='500') {
+                  console.log("system error");
+                }
+              });
+    };
+  });
