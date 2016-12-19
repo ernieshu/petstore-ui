74a237b565c42a1a9136278ec66e4da3ee2e7ba4
Ernie Shu <ernie.shu@gmail.com>
2016-12-19 10:54:47 -0500

    ES - deprecate about.

--- app/scripts/services/petservice.js
+++ app/scripts/services/petservice.js
@@ -8,6 +8,28 @@
  * Service in the petstoreUiApp.
  */
 angular.module('petstoreUiApp')
-  .service('petservice', function () {
+  .service('petservice', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+
+    this.findAPet = function(searchPetId) {
+      var url = 'pet/' + searchPetId;
+      // console.log("url:" + url);
+      $http.get(url)
+        .then(function(response) {
+          var returnPet = response.data
+          console.log("Retrieving info for pet name / pet id : " + returnPet.name + "/" + returnPet.id);
+          // if we've gotten the pet, map it back to the GUI
+          return returnPet;
+        }, function(error) {
+          if (error.status===404) {
+            return null; // indicates no pet found
+          }
+          else {
+          	// if it is not a 'not found', simply throw an error
+            throw error;
+          }
+        });
+    };
+
+
   });
